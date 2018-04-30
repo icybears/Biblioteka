d173b05c08c435adfd35488fb34ec75beb7c8fa5
I. Saber <isaber23@outlook.com>
2018-04-30 21:00:37 +0100

    ui enhancement

--- resources/views/book.blade.php
+++ resources/views/book.blade.php
@@ -13,15 +13,13 @@
             </div>
 
             <div class="col s12 m6">
-              <div class="component">
-                    <ul class="collection with-header">
+                    <ul class="collection with-header component">
                         <li class="collection-header"><h4>{{ $book->title }}</h4></li>
                         <li class="collection-item"><strong>Author:</strong>&nbsp;{{ $book->author}}</li>
                         <li class="collection-item"><strong>Number of pages:</strong>&nbsp;{{ $book->pages }}</li>
                         <li class="collection-item"><strong>Year of Publication:</strong>&nbsp;{{ $book->year }}</li>
                         <li class="collection-item"><strong>Language:</strong>&nbsp;{{ $book->language }}</li>
                     </ul>
-              </div>
             </div>
 
         </div>
